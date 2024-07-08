f8203b0ab3b30eeb690beb70ffe6e6d472949d05
Leandro Macedo <leandro.smacedo@Live.com>
2024-07-08 12:14:25 +0100

    feat: add new blocks

---
+++ src/components/blocks/defaultBlock.tsx
@@ -0,0 +1,238 @@
+import { CopyTheme } from "@/components/copy-theme";
+import { Checkbox } from "@/components/ui/checkbox"
+import { AlertCircle, ArrowRight, Terminal } from "@/components/icons";
+import { Sidebar } from "@/components/sidebar";
+import { ToastDemo } from "@/components/toast-demo";
+
+import { SaveThemeDialog } from "@/components/save-theme-dialog";
+import { SavedThemes } from "@/components/saved-themes";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Slider } from "@/components/ui/slider";
+import { Switch } from "@/components/ui/switch";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function DefaultBlock() {
+    return (
+        <>
+            <section>
+                {/* First Row */}
+                <div className="grid grid-flow-row auto-rows-max gap-1">
+                    <Alert>
+                        <Terminal />
+                        <AlertTitle>Heads up!</AlertTitle>
+                        <AlertDescription>
+                            You can add components to your app using the cli.
+                        </AlertDescription>
+                    </Alert>
+
+                    <Alert variant="destructive">
+                        <AlertCircle />
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription>
+                            Your session has expired. Please log in again.
+                        </AlertDescription>
+                    </Alert>
+                </div>
+
+                {/* Second Row */}
+                <div className="mt-2 grid grid-cols-1 gap-2 lg:grid-cols-2">
+                    <Card className="rounded-lg">
+                        <CardHeader>
+                            <CardTitle>Create project</CardTitle>
+                            <CardDescription>
+                                Deploy your new project in one-click.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="name">Name</Label>
+                                        <Input id="name" placeholder="Name of your project" />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="framework">Framework</Label>
+                                        <Select>
+                                            <SelectTrigger id="framework">
+                                                <SelectValue placeholder="Select" />
+                                            </SelectTrigger>
+                                            <SelectContent position="popper">
+                                                <SelectItem value="next">Next.js</SelectItem>
+                                                <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                                <SelectItem value="astro">Astro</SelectItem>
+                                                <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button variant="outline">Cancel</Button>
+                            <Button>Deploy</Button>
+                        </CardFooter>
+                    </Card>
+                    <div className="flex flex-col gap.2">
+                        <div className="flex items-center space-x-2">
+                            <Checkbox id="terms" />
+                            <label
+                                htmlFor="terms"
+                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                                Accept terms and conditions
+                            </label>
+                        </div>
+                    </div>
+                    <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It adheres to the WAI-ARIA design pattern.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger>Is it styled?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It comes with default styles that matches the other
+                                components&apos; aesthetic.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>Is it animated?</AccordionTrigger>
+                            <AccordionContent>
+                                Yes. It's animated by default, but you can disable it if you
+                                prefer.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
+
+                {/* Third Row */}
+                <Table className="mt-2">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Invoice</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Method</TableHead>
+                            <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">INV001</TableCell>
+                            <TableCell>Paid</TableCell>
+                            <TableCell>Credit Card</TableCell>
+                            <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">INV002</TableCell>
+                            <TableCell>Pending</TableCell>
+                            <TableCell>Bank Transfer</TableCell>
+                            <TableCell className="text-right">$550.00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={3}>Total</TableCell>
+                            <TableCell className="text-right">$800.00</TableCell>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+
+                {/* Fourth Row */}
+                <div className="mt-2 grid grid-cols-2 gap-2 lg:grid-cols-[1fr_auto_1fr_1fr_1fr]">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Button variant="outline">Hover</Button>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Add to library</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                    <div className="flex items-center space-x-2">
+                        <Switch id="airplane-mode" />
+                        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+                    </div>
+
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Edit Profile</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                                <DialogDescription>
+                                    This action cannot be undone. This will permanently delete
+                                    your account and remove your data from our servers.
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+
+                    <Slider defaultValue={[50]} max={100} step={1} />
+
+                    <ToastDemo className="col-span-2 lg:col-span-1" />
+                </div>
+
+                {/* Fifth Row */}
+                <div className="mt-2 grid grid-cols-2 gap-2 lg:grid-cols-6">
+                    <Button variant="default">Default</Button>
+                    <Button variant="secondary">Secondary</Button>
+                    <Button variant="outline">Outline</Button>
+                    <Button variant="ghost">Ghost</Button>
+                    <Button variant="destructive">Destructive</Button>
+                    <Button variant="link">Link</Button>
+                </div>
+            </section>
+        </>
+    )
+}
